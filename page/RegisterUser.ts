5705ec9d896b137dd283c92d080dd0b337ffadd7
Lucas Camargo <nascimentocamargolucas@gmail.com>
2026-01-14 19:24:25 -0300

    Developing project.

--- page/RegisterUser.ts
+++ page/RegisterUser.ts
@@ -12,5 +12,27 @@ export class RegisterUser {
     await this.page.getByTestId('cadastrar').click();
   }
 
+  //preenceher campo Nome
+  async fillNome(nome:string) {
+    await this.page.getByTestId('nome').fill(nome);
+  }
+
+  //preenceher campo Email
+  async fillEmail(email:string) {
+    await this.page.getByTestId('email').fill(email);
+  }
+
+  //preenceher campo Senha
+  async fillSenha(senha:string) {
+    await this.page.getByTestId('password').fill(senha);
+  }
+
+  //marcar checkbox Administrador
+  async checkAdministrador() {
+    await this.page.getByTestId('checkbox').check();
+  }
+
+  
+
  
 }
--- tests/e2e/newUser.spec.ts
+++ tests/e2e/newUser.spec.ts
@@ -1,7 +1,13 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker';
 import { RegisterUser } from '../../page/RegisterUser';
+import { register } from 'module';
 
 let registerUser: RegisterUser;
+
+const nome = faker.person.fullName();
+const email = faker.internet.email();
+const senha = faker.internet.password({ length: 12 });
 
 test.beforeEach(async ({ page }) => {
   registerUser = new RegisterUser(page)
@@ -13,6 +19,12 @@ test.beforeEach(async ({ page }) => {
 
 test('Register User', async ({ page }) => {
 
+  registerUser.fillNome(nome);
+  registerUser.fillEmail(email);
+  registerUser.fillSenha(senha);
+  registerUser.checkAdministrador();
+  
+  
   
 });
 
